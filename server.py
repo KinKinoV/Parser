0b736661bdd35d08ae3511cef58ca7e6a50f4a94
KinKinoV <mr.kolbasa.ask@gmail.com>
2022-07-18 10:56:46 +0300

    Completed server app for data input

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app.jinja_env.globals['static'] = (
 )
 
 def start():
-    app.run()
+    app.run(host='0.0.0.0')
 
 @app.route('/', methods=['GET', 'POST'])
 def get_first_data():
@@ -27,52 +27,53 @@ def get_first_data():
 @app.route('/tag_definition/<mp_amm>/<pp_amm>/<tp_amm>/<ftp_amm>', methods=["GET", "POST"])
 def get_tag_definition(mp_amm, pp_amm, tp_amm, ftp_amm):
     if request.method == 'POST':
+        regex_pattern = '/~/'
         message_parameter = {}
         if mp_amm == '1':
             if request.form.get(f'message_parameter{0}_regex') == 'True':
-                message_parameter[request.form.get(f'message_parameter{0}_name')] = re.compile(request.form.get(f'message_parameter{0}_value')).pattern
+                message_parameter[request.form.get(f'message_parameter{0}_name')] = regex_pattern + request.form.get(f'message_parameter{0}_value')
             else:
                 message_parameter[request.form.get(f'message_parameter{0}_name')] = request.form.get(f'message_parameter{0}_value')
         else:
             for i in range(int(mp_amm)):    
                 if request.form.get(f'message_parameter{i}_regex') == 'True':
-                    message_parameter[request.form.get(f'message_parameter{i}_name')] = re.compile(request.form.get(f'message_parameter{i}_value')).pattern
+                    message_parameter[request.form.get(f'message_parameter{i}_name')] = regex_pattern + request.form.get(f'message_parameter{i}_value')
                 else:
                     message_parameter[request.form.get(f'message_parameter{i}_name')] = request.form.get(f'message_parameter{i}_value')
         pagination_parameter = {}
         if pp_amm == '1':
             if request.form.get(f'pagination_parameter{0}_regex') == 'True':
-                pagination_parameter[request.form.get(f'pagination_parameter{0}_name')] = re.compile(request.form.get(f'pagination_parameter{0}_value')).pattern
+                pagination_parameter[request.form.get(f'pagination_parameter{0}_name')] = regex_pattern + request.form.get(f'pagination_parameter{0}_value')
             else:
                 pagination_parameter[request.form.get(f'pagination_parameter{0}_name')] = request.form.get(f'pagination_parameter{0}_value')
         else:
             for i in range(int(pp_amm)):
                 if request.form.get(f'pagination_parameter{i}_regex') == 'True':
-                    pagination_parameter[request.form.get(f'pagination_parameter{i}_name')] = re.compile(request.form.get(f'pagination_parameter{i}_value')).pattern
+                    pagination_parameter[request.form.get(f'pagination_parameter{i}_name')] = regex_pattern + request.form.get(f'pagination_parameter{i}_value')
                 else:
                     pagination_parameter[request.form.get(f'pagination_parameter{i}_name')] = request.form.get(f'pagination_parameter{i}_value')
         thread_post_parameter = {}
         if tp_amm == '1':
             if request.form.get(f'thread_post_parameter{0}_regex') == 'True':
-                thread_post_parameter[request.form.get(f'thread_post_parameter{0}_name')] = re.compile(request.form.get(f'thread_post_parameter{0}_value')).pattern
+                thread_post_parameter[request.form.get(f'thread_post_parameter{0}_name')] = regex_pattern + request.form.get(f'thread_post_parameter{0}_value')
             else:
                 thread_post_parameter[request.form.get(f'thread_post_parameter{0}_name')] = request.form.get(f'thread_post_parameter{0}_value')
         else:
             for i in range(int(tp_amm)):
                 if request.form.get(f'thread_post_parameter{i}_regex') == 'True':
-                    thread_post_parameter[request.form.get(f'thread_post_parameter{i}_name')] = re.compile(request.form.get(f'thread_post_parameter{i}_value')).pattern
+                    thread_post_parameter[request.form.get(f'thread_post_parameter{i}_name')] = regex_pattern + request.form.get(f'thread_post_parameter{i}_value')
                 else:
                     thread_post_parameter[request.form.get(f'thread_post_parameter{i}_name')] = request.form.get(f'thread_post_parameter{i}_value')
         forum_threads_parameter = {}
         if ftp_amm == '1':
             if request.form.get(f'forum_threads_parameter{0}_regex') == 'True':
-                forum_threads_parameter[request.form.get(f'forum_threads_parameter{0}_name')] = re.compile(request.form.get(f'forum_threads_parameter{0}_value')).pattern
+                forum_threads_parameter[request.form.get(f'forum_threads_parameter{0}_name')] = regex_pattern + request.form.get(f'forum_threads_parameter{0}_value')
             else:
                 forum_threads_parameter[request.form.get(f'forum_threads_parameter{0}_name')] = request.form.get(f'forum_threads_parameter{0}_value')
         else:
             for i in range(int(ftp_amm)):
                 if request.form.get(f'forum_threads_parameter{i}_regex') == 'True':
-                    forum_threads_parameter[request.form.get(f'forum_threads_parameter{i}_name')] = re.compile(request.form.get(f'forum_threads_parameter{i}_value')).pattern
+                    forum_threads_parameter[request.form.get(f'forum_threads_parameter{i}_name')] = regex_pattern + request.form.get(f'forum_threads_parameter{i}_value')
                 else:
                     forum_threads_parameter[request.form.get(f'forum_threads_parameter{i}_name')] = request.form.get(f'forum_threads_parameter{i}_value')
         
@@ -97,5 +98,4 @@ def get_tag_definition(mp_amm, pp_amm, tp_amm, ftp_amm):
     }
     return render_template("needed_data.html", context=context_)
 
-if __name__ == '__main__':
-    start()
+start()
