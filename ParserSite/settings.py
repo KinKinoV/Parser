e77d9d31be24e2b6bda30b78777390a30bab672e
KinKinoV <mr.kolbasa.ask@gmail.com>
2023-04-18 15:36:19 +0300

    Settings to the whole django app

--- ParserSite/settings.py
+++ ParserSite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = djangoSecretKey
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.158', 'localhost', '46.33.36.48']
+ALLOWED_HOSTS = allowed_ip
 
 
 # Application definition
