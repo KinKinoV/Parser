0c4784f150306953894d63d4e5f44c74a6f3c2d7
KinKinoV <mr.kolbasa.ask@gmail.com>
2023-04-21 11:34:16 +0300

    Made coresponding changes after creating Nickname model

--- parser/admin.py
+++ parser/admin.py
@@ -2,32 +2,19 @@ from django.contrib import admin
 from parser.models import *
 # Register your models here.
 
-class TgHandlerInline(admin.StackedInline):
-    model = TgHandler
-
-class ForumNicknameInline(admin.StackedInline):
-    model = ForumNickname
+class NicknameInline(admin.StackedInline):
+    model = Nickname
 
 @admin.register(Forum)
 class ForumAdmin(admin.ModelAdmin):
     list_display = ('link', 'parseConfigs')
     fields = ['link', 'parseConfigs']
-    inlines = [TgHandlerInline, ForumNicknameInline]
+    inlines = [NicknameInline]
 
-
-class TgHandlerNicknamesInline(admin.StackedInline):
-    model = ForumNickname.tgHandlers.through
-
-@admin.register(TgHandler)
-class TgHandlerAdmin(admin.ModelAdmin):
-    list_display = ('handler', 'forumOrigin')
-    fields = ['forumOrigin', 'handler']
-
-    inlines = [TgHandlerNicknamesInline]
-
-@admin.register(ForumNickname)
-class ForumNicknameAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Nickname)
+class NicknameAdmin(admin.ModelAdmin):
+    list_display = ('handler', 'user_id', 'nicknames', 'forumOrigin')
+    fields = ['forumOrigin', 'handler', 'user_id', 'nicknames']
 
 @admin.register(BannedWord)
 class BannedWordAdmin(admin.ModelAdmin):
