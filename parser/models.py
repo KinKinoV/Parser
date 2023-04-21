6d238f1642dbc3c9c2b5eec8ef8b82786dc9216e
KinKinoV <mr.kolbasa.ask@gmail.com>
2023-04-21 11:33:37 +0300

    United TgHandler and ForumNickname into the Nickname model

--- parser/models.py
+++ parser/models.py
@@ -7,21 +7,15 @@ class Forum(models.Model):
     def __str__(self) -> str:
         return self.link
 
-# Telegram @... handlers
-class TgHandler(models.Model):
+# Telegram @... handlers and their related forum nicknames
+class Nickname(models.Model):
     handler = models.CharField(max_length=32, unique=True)
+    user_id = models.CharField(max_length=64, unique=True, null=True)
+    nicknames = models.TextField(null=True)
     forumOrigin = models.ForeignKey(to = Forum, to_field = 'link', on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.handler
-
-class ForumNickname(models.Model):
-    nickname = models.CharField(max_length=50)
-    tgHandlers = models.ManyToManyField(to = TgHandler, blank=True, related_name='nicknames')
-    forumOrigin = models.ForeignKey(to = Forum, to_field='link', on_delete= models.CASCADE)
-
-    def __str__(self) -> str:
-        return self.nickname
 
 class BannedWord(models.Model):
     filter = models.CharField(max_length=50, unique=True, help_text='Word/statement to exclude in search')
